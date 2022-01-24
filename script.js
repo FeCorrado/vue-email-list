211fe244f536a610796232917de6c2e9fbd5a258
Corrado.Ferra <corradoferra@gmail.com>
2022-01-24 16:42:12 +0100

    bonus

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 new Vue({
-    el:"#app",
-    data: {
-        emails: [],
-    },
-  
-    mounted(){
-        
-        for (let i = 0; i < 10 ; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-            if (this.emails.includes(result.data.response)) {
-            }else{
-                this.emails.push(result.data.response)
-            }
-            })
-            
-        }
+  el: "#app",
+  data: {
+    emails: [],
+  },
 
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((result) => {
+          if (!this.emails.includes(result.data.response)) {
+            this.emails.push(result.data.response);
+          }
+        });
     }
-
+  },
 });
